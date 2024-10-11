1fbef18570c0d04757f27b6fd5a07dd9a7624ca8
Nikola43 <pauloxti@gmail.com>
2024-10-11 18:55:04 +0200

    addede batch

--- main.go
+++ main.go
@@ -1,63 +1,67 @@
 package main
 
 import (
-	"crypto/ecdsa"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
+    "crypto/ecdsa"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "os"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/crypto"
-	"golang.org/x/crypto/sha3"
+    "github.com/ethereum/go-ethereum/common/hexutil"
+    "github.com/ethereum/go-ethereum/crypto"
+    "golang.org/x/crypto/sha3"
 )
 
 type Wallet struct {
-	PublicKey  string `json:"PublicKey"`
-	PrivateKey string `json:"PrivateKey"`
+    PublicKey  string `json:"PublicKey"`
+    PrivateKey string `json:"PrivateKey"`
 }
 
 func main() {
-	GenerateWallet()
+    numWallets := 10 // Number of wallets to generate
+    for i := 0; i < numWallets; i++ {
+        GenerateWallet(i)
+    }
 }
 
-func GenerateWallet() {
+func GenerateWallet(index int) {
 
-	privateKey, err := crypto.GenerateKey()
-	if err != nil {
-		log.Fatal(err)
-	}
+    privateKey, err := crypto.GenerateKey()
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	privateKeyBytes := crypto.FromECDSA(privateKey)
-	publicKey := privateKey.Public()
-	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-	if !ok {
-		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
-	}
+    privateKeyBytes := crypto.FromECDSA(privateKey)
+    publicKey := privateKey.Public()
+    publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+    if !ok {
+        log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+    }
 
-	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+    publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
 
-	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+    address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
 
-	hash := sha3.NewLegacyKeccak256()
-	hash.Write(publicKeyBytes[1:])
+    hash := sha3.NewLegacyKeccak256()
+    hash.Write(publicKeyBytes[1:])
 
-	wallet := Wallet{
-		PublicKey:  address,
-		PrivateKey: hexutil.Encode(privateKeyBytes)[2:],
-	}
+    wallet := Wallet{
+        PublicKey:  address,
+        PrivateKey: hexutil.Encode(privateKeyBytes)[2:],
+    }
 
-	dirname, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatal(err)
-	}
+    projectDir, err := os.Getwd()
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	if _, err := os.Stat(dirname + "/wallets/"); os.IsNotExist(err) {
-		os.Mkdir(dirname+"/wallets/", 0755)
-	}
+    walletDir := projectDir + "/wallets/"
+    if _, err := os.Stat(walletDir); os.IsNotExist(err) {
+        os.Mkdir(walletDir, 0755)
+    }
 
-	file, _ := json.MarshalIndent(wallet, "", " ")
-	_ = ioutil.WriteFile(dirname+"/wallets/"+address+".json", file, 0755)
-	fmt.Println("Wallet generated: ", wallet)
+    file, _ := json.MarshalIndent(wallet, "", " ")
+    _ = ioutil.WriteFile(fmt.Sprintf("%s%s_%d.json", walletDir, address, index), file, 0755)
+    fmt.Println("Wallet generated: ", wallet)
 }
